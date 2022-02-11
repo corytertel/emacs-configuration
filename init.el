360bbcebd71d4fd47055d8b1b7fb1c6fabf14f8a
cory <cory@slack.example.com>
2022-02-10 18:34:50 -0700

    emacs update

--- init.el
+++ init.el
@@ -4,13 +4,24 @@
 
 (scroll-bar-mode -1) ; Disables the visible scrollbar
 (tool-bar-mode -1)   ; Disables the toolbar
+(menu-bar-mode -1)   ; Disables the menubar
 (tooltip-mode -1)    ; Disables tooltips
 (set-fringe-mode 10) ; Gives some breathing room
 
 (setq visible-bell t) ; Enables the visible bell
 
 ;; Sets the font. Height is a percentage
-(set-face-attribute 'default nil :font "Hack Nerd Font" :height 100)
+;(set-face-attribute 'default nil :font "Hack" :height 100)
+(set-face-attribute 'default nil
+		    :family "Roboto Mono" :weight 'light :height 100)
+(set-face-attribute 'bold nil
+		    :family "Roboto Mono" :weight 'regular)
+(set-face-attribute 'italic nil
+		    :family "Victor Mono" :weight 'semilight :slant 'italic)
+(set-fontset-font t 'unicode
+		  (font-spec :name "RobotoMono Nerd Font" :size 16) nil)
+(set-fontset-font t '(#xe000 . #xffdd)
+		  (font-spec :name "RobotoMono Nerd Font" :size 12) nil)
 
 (load-theme 'tango)
 
@@ -34,6 +45,16 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
+(column-number-mode)
+(global-display-line-numbers-mode t)
+
+;; Disable line numbers for some modes
+(dolist (mode '(org-mode-hook
+		term-mode-hook
+		shell-mode-hook
+		eshell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
 (use-package command-log-mode)
 
 (use-package ivy
@@ -54,23 +75,60 @@
   :config
   (ivy-mode 1))
 
+(use-package swiper)
+
 (use-package doom-modeline
   :ensure t
   :init (doom-modeline-mode 1)
-  :custom ((doom-modeline-height 15)))
+  :custom ((doom-modeline-height 25)))
 
-;; (custom-set-variables
-;;  ;; custom-set-variables was added by Custom.
-;;  ;; If you edit it by hand, you could mess it up, so be careful.
-;;  ;; Your init file should contain only one such instance.
-;;  ;; If there is more than one, they won't work right.
-;;  '(ansi-color-faces-vector
-;;    [default default default italic underline success warning error])
-;;  '(global-command-log-mode t)
-;;  '(package-selected-packages '(doom-modeline ivy command-log-mode use-package)))
-;; (custom-set-faces
-;;  ;; custom-set-faces was added by Custom.
-;;  ;; If you edit it by hand, you could mess it up, so be careful.
-;;  ;; Your init file should contain only one such instance.
-;;  ;; If there is more than one, they won't work right.
-;;  )
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config
+  (setq which-key-idle-delay 0.3))
+
+(use-package ivy-rich
+  :init
+  (ivy-rich-mode 1))
+
+(use-package counsel
+  :bind (("M-x" . counsel-M-x)
+	 ("C-x b" . counsel-ibuffer)
+	 ("C-x C-f" . counsel-find-file)
+	 :map minibuffer-local-map
+	 ("C-r" . 'counsel-minibuffer-history))
+  :config
+  (setq ivy-initial-inputs-alist nil)) ;; Don't start searches with ^
+
+(use-package helpful
+  :ensure
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-key] . helpful-key))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline sucess warning error])
+ '(global-command-log-mode t)
+ '(ivy-mode t)
+ '(package-selected-packages
+   '(ivy-rich which-key rainbow-delimiters counsel swiper use-package ivy doom-modeline command-log-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
