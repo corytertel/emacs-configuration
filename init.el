a81e0a21e7a6634b5da17d5e9139e0287bdabfb0
cory <cory@slack.example.org>
2022-02-11 05:03:58 -0700

    evil mode

--- init.el
+++ init.el
@@ -13,7 +13,7 @@
 ;; Sets the font. Height is a percentage
 ;(set-face-attribute 'default nil :font "Hack" :height 100)
 (set-face-attribute 'default nil
-		    :family "Roboto Mono" :weight 'light :height 100)
+		    :family "Roboto Mono" :weight 'light :height 140)
 (set-face-attribute 'bold nil
 		    :family "Roboto Mono" :weight 'regular)
 (set-face-attribute 'italic nil
@@ -77,6 +77,84 @@
 
 (use-package swiper)
 
+;; Basic Keybind
+(global-set-key (kbd "C-M-j") 'counsel-switch-buffer)
+
+;; Define a binding just for a certain mode
+;;(define-key emacs-lisp-mode-map (kbd "C-x M-t") 'counsel-load-theme)
+
+;; Declare keybinds in a more consise way
+(use-package general
+  :config
+  (general-evil-setup t)
+  (general-create-definer cory/leader-keys
+			  :keymaps '(normal insert visual emacs)
+			  :prefix "SPC"
+			  :global-prefix "C-SPC")
+  (cory/leader-keys
+    ;; Search (M-x alternative)
+    "SPC" '(counsel-M-x :which-key "M-x")
+
+    ;; Buffers
+    "b"  '(:ignore t :which-key "buffers")
+    "bb" '(counsel-switch-buffer :which-key "switch buffer")
+    "be" '(eval-buffer :which-key "eval buffer")
+
+    ;; Files
+    "f"  '(:ignore t :which-key "files")
+    "ff" '(counsel-find-file :which-key "find-file")
+
+    ;; Git
+    "g"  '(:ignore t :which-key "git")
+    
+    ;; Toggles
+   "t"  '(:ignore t :which-key "toggles")
+   "ts" '(hydra-text-scale/body :which-key ":scale-text")
+   "tt" '(counsel-load-theme :which-key "choose theme")
+   ))
+
+(defun cory/evil-hook ()
+  (dolist (mode '(custom-mode
+		  eshell-mode
+		  git-rebase-mode
+		  erc-mode
+		  circe-server-mode
+		  circe-chat-mode
+		  circe-query-mode
+		  sauron-mode
+		  term-mode))
+    (add-to-list 'evil-emacs-state-modes mode)))
+
+(use-package evil
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  (setq evil-want-C-u-scroll t)
+  (setq evil-want-C-i-jump nil)
+  ;(setq evil-undo-system 'undo-tree)
+  :hook (evil-mode . cory/evil-hook)
+  :config
+  (evil-mode 1)
+  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
+
+  ;; Use visual line motions even outside of visual-line-mode buffers
+  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+
+  (evil-set-initial-state 'messages-buffer-mode 'normal)
+  (evil-set-initial-state 'dashboard-mode 'normal))
+
+(use-package evil-collection
+  :after evil
+  :config
+  (evil-collection-init))
+
+;(use-package undo-tree)
+;(global-undo-tree-mode)
+
+(use-package all-the-icons)
+
 (use-package doom-modeline
   :ensure t
   :init (doom-modeline-mode 1)
@@ -115,6 +193,23 @@
   ([remap describe-variable] . counsel-describe-variable)
   ([remap describe-key] . helpful-key))
 
+(use-package centaur-tabs
+  :demand
+  :config
+  (centaur-tabs-mode t)
+  (setq centaur-tabs-style "bar")
+  :bind
+  ("C-<prior>" . centaur-tabs-backward)
+  ("C-<next>" . centaur-tabs-forward))
+
+(use-package hydra)
+
+(defhydra hydra-text-scale (:timeout 4)
+  "scale text"
+  ("j" text-scale-increase "in")
+  ("k" text-scale-decrease "out")
+  ("f" nil "finished" :exit t))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -125,7 +220,7 @@
  '(global-command-log-mode t)
  '(ivy-mode t)
  '(package-selected-packages
-   '(ivy-rich which-key rainbow-delimiters counsel swiper use-package ivy doom-modeline command-log-mode)))
+   '(undo-tree undo-fu evil-collection evil general ivy-rich which-key rainbow-delimiters counsel swiper use-package ivy doom-modeline command-log-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
