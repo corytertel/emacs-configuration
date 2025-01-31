c09d4c04939ff7fbc0b87e186b9f4928d54b35bf
corytertel <ctertel@comcast.net>
2025-01-31 16:46:12 -0700

    feat: Add lsp-bridge configuration

---
+++ init-lsp.el
@@ -0,0 +1,16 @@
+
+;; Use lsp-bridge for it's speed.
+;; Unfortunately, lsp-bridge is neither built-in, nor is it pure elisp.
+
+;; lsp-bridge requires python dependenices:
+;; pip3 install epc orjson sexpdata six setuptools paramiko rapidfuzz watchdog packaging
+
+;; lsp-bridge requires markdown-mode and yasnippet as dependencies
+(pkg 'markdown-mode)
+(pkg 'yasnippet)
+(require 'yasnippet)
+(yas-global-mode 1)
+
+(pkg-git 'lsp-bridge "https://github.com/manateelazycat/lsp-bridge")
+(require 'lsp-bridge)
+(global-lsp-bridge-mode)
