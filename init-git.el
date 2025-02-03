1f8434175648ed6a921d43502fde0c76e51bc7d3
corytertel <ctertel@comcast.net>
2025-02-03 15:39:34 -0700

    feat: Add magit

---
+++ init-git.el
@@ -0,0 +1,3 @@
+
+(pkg 'magit)
+(require 'magit)
--- init.el
+++ init.el
@@ -28,7 +28,40 @@
 ;;; I also use tmux binds for window management because these days tmux seems to be
 ;;; the de-facto window-manager/terminal-multiplexer.
 
+;;; P.S. One method of crafting this config is to learn the current config in it's entirety before I
+;;; add another package. Don't add a bunch of packages at once and try to learn them
+;;; all at once. Just add one at a time, and learn it thoroughly. Even if you have a
+;;; bunch of packages which you have in mind which you want to use, just go one at a time.
+
+;;; P.P.S. The idea is that this config is as close being a superset
+;;; of vim as possible.  If vim supports an action, this should also
+;;; support that action. And if both evil and vanilla emacs provide
+;;; that action, it should prefered to do it the vim way. An example
+;;; would be killing a buffer. One could kill a buffer the emacs way,
+;;; C-x k, or the vim way, :q. The vim way of :q should be
+;;; used. Because C-x k should not be used, it may be unbound or
+;;; rebound to a different action.
+
+;;; Packages to add in the future (try to prioritize built-in packages):
+;;; - use-package w/ lazy loading
+;;; - Magit
+;;; - in emacs terminal
+;;; - tramp
+;;; - use dired
+;;; - embark + wgrep (and rg.el)
+;;; - modeline
+;;; - theme
+;;; - use bookmarks, registers, and marks
+;;; - learn the undo ring
+;;; - use the emacs editing features
+;;; - avy
+;;; - workspaces
+;;; - undo (either built-in undo ring or undo tree)
+;;; - sudo-edit
+;;; - info mode
+
 ;; Set up package.el to work with MELPA
+;; Use a MELPA mirror because some places block melpa.org
 (require 'package)
 (add-to-list 'package-archives
              '("melpa" . "https://www.mirrorservice.org/sites/melpa.org/packages/"))
@@ -52,6 +85,9 @@
 
 ;; Lsp
 (load "~/.emacs.d/init-lsp.el")
+
+;; Git
+(load "~/.emacs.d/init-git.el")
 
 ;; Web dev
 (load "~/.emacs.d/init-web-dev.el")
